83de8ef19903f5ae8169d2610646a65a8d3013c5
titi73370 <thib.chami@gmail.com>
2020-05-14 14:08:36 +0200

    update client tuple

--- Afficheur.py
+++ Afficheur.py
@@ -14,9 +14,10 @@ class Afficheur(Thread):
         self.phrase = phrase
 
     def run(self):
+
         """Code à exécuter pendant l'exécution du thread."""
         i = 0
-        while i < 3:
+        while i < 1:
             with verrou:
                 for lettre in self.phrase:
                     sys.stdout.write(lettre)
@@ -27,14 +28,28 @@ class Afficheur(Thread):
             i += 1
 
 # Création des threads
-tup1 = ("pain", "frite", "avocat")
-thread_1 = Afficheur(" Je vous propose " + tup1[random.randint(0, 2)] )
-thread_2 = Afficheur(" second Thread ")
+
+thread_1 = Afficheur("1-" + " Je vous propose " + Main.tup1[0] + ", les caracteristiques sont: " + Main.tup1[1] + ", le produit vous sera délivré en " + Main.tup1[2]
+                     + " jours pour une quantité de " + Main.tup1[3] + " et le cout de production vous sera de " + str(random.randint(1, 50)) + " euros" + "\n")
+thread_2 = Afficheur("2-" + " Je vous propose " + Main.tup1[0] + ", les caracteristiques sont: " + Main.tup1[1] + ", le produit vous sera délivré en " + Main.tup1[2]
+                     + " jours pour une quantité de " + Main.tup1[3] + " et le cout de production vous sera de " + Main.tup1[4] + " euros" + "\n")
+thread_3 = Afficheur("3-" + " Je vous propose " + Main.tup1[0] + ", les caracteristiques sont: " + Main.tup1[1] + ", le produit vous sera délivré en " + Main.tup1[2]
+                     + " jours pour une quantité de " + Main.tup1[3] + " et le cout de production vous sera de " + Main.tup1[4] + " euros" + "\n")
+thread_4 = Afficheur("4-" + " Je vous propose " + Main.tup1[0] + ", les caracteristiques sont: " + Main.tup1[1] + ", le produit vous sera délivré en " + Main.tup1[2]
+                     + " jours pour une quantité de " + Main.tup1[3] + " et le cout de production vous sera de " + Main.tup1[4] + " euros" + "\n")
+thread_5 = Afficheur("5-" + " Je vous propose " + Main.tup1[0] + ", les caracteristiques sont: " + Main.tup1[1] + ", le produit vous sera délivré en " + Main.tup1[2]
+                     + " jours pour une quantité de " + Main.tup1[3] + " et le cout de production vous sera de " + Main.tup1[4] + " euros" + "\n")
 
 # Lancement des threads
 thread_1.start()
 thread_2.start()
+thread_3.start()
+thread_4.start()
+thread_5.start()
 
 # Attend que les threads se terminent
 thread_1.join()
 thread_2.join()
+thread_3.join()
+thread_4.join()
+thread_5.join()
